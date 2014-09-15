20d07c29c8672f90024eb5da99581e13341f67c4
Zhou Shaowen <shaowen@powerme.com.sg>
2014-09-15 14:29:52 +0800

    initial commit

---
+++ CS2106_proj2_ProcMgr/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CS2106_proj2_ProcMgr
+//
+//  Created by Zsw on 15/9/14.
+//  Copyright (c) 2014 Zsw. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
