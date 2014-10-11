6979553b6aaf7f33781d52b5f6b4e8046deaf3a6
Zhou Shaowen <shaowen@powerme.com.sg>
2014-10-12 01:29:45 +0800

    Created the project

---
+++ CS2106_proj_VM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS2106_proj_VM
+//
+//  Created by Zsw on 12/10/14.
+//  Copyright (c) 2014 Zsw. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
